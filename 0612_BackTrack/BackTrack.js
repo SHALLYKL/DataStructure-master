6664467bd6393e8d9c6ad2e352bd7b70da5a0623
SHALLYKL <657669149@qq.com>
2020-09-09 21:07:43 +0800

    回溯

---
+++ 0612_BackTrack/BackTrack.js
@@ -0,0 +1,79 @@
+//回溯算法的通用框架已写在markdown里面了。
+
+var res = [];
+/**
+ * @name: 输入一组不重复的数字，返回它们的全排列
+ * @param {Array}
+ * @return {Array} 全排列数组
+ */
+function permute(arr){
+    backtrack(arr,[]);
+}
+/**
+ * @name: 
+ * @param {type} 
+ * @param {Array} track 路径
+ * @return {type} 
+ */
+//路径：记录在track中
+//选择列表：arr中不存在与track的那些元素
+//结束条件：arr中的元素全都在track中出现
+function backtrack(arr,track){
+    if(track.length==arr.length){
+        res.push([].concat(track));
+        return;
+    }
+    for(let i=0;i<arr.length;i++){
+        //排除不合适的选择
+        if(track.indexOf(arr[i])!=-1) continue;
+        //做选择
+        track.push(arr[i]);
+        //进入下一层决策树
+        backtrack(arr,track);
+        track.pop();
+    }
+}
+
+var testArr = [1,2,3];
+permute(testArr);
+
+var queArr = [];
+/**
+ * @name: N皇后问题 输入棋盘边长n，返回所有合法的位置
+ * @param {type} 
+ * @return {type} 
+ */
+function NQueens(n){
+    //0表示空，1表示皇后，初始化空棋盘
+    let arr = [];
+    arr.length = n;
+    for(let i=0;i<n;i++){
+        arr[i] = [];
+        arr[i].fill(0);
+    }
+    NQueensBacktrack(arr,0);
+    return queArr;
+}
+//路径：arr中小于row的那些都已经成功放置了皇后
+//选择列表：第row行的所有列都是放置皇后的选择
+//结束条件：row超过arr的最后一行
+function NQueensBacktrack(arr,row){
+    if(row == arr.length){
+        queArr.push(arr);
+        return;
+    }
+    let n = arr[row].length;
+    for(let col = 0;col<n;col++){
+        //排除不合法选择
+        if(!isValid(arr,row,col)) continue;
+        //做选择
+        arr[row][col] = 1;
+        //进入下一行决策
+        backtrack(arr,row+1);
+        //撤销选择
+        arr[row][col] = 0;
+    }
+}
+function isValid(arr,row,col){
+    
+}
--- 10_Sort/HeapSort.js
+++ 10_Sort/HeapSort.js
@@ -3,24 +3,6 @@
  * @param {Array} arr
  */
 function HeapSort(arr){
-    //对顺序表arr做简单选择排序
-    let j;
-    for(let i=0;i<arr.length;i++){
-        // j = SelectMin(arr,i);
-        let tmp = arr[i];
-        j = i;
-        for(let m=i;m<arr.length;m++){
-           if(arr[m]<tmp){
-               tmp = arr[m];
-               j = m;
-           }
-        }
-        if(i!=j){
-            let tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
-        }
-    }
+    //对顺序表arr做简单选择排序 
+
 }
-var testArr = [7, 5, 3, 10, 2, 4, 1];
-console.log(HeapSort(testArr));
