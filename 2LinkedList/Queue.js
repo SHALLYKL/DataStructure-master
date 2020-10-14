8b70ce853fcff1372d380647affc4e8e6a699bc2
SHALLYKL <657669149@qq.com>
2020-10-14 23:57:32 +0800

    循环队列的实现

--- 2LinkedList/Queue.js
+++ 2LinkedList/Queue.js
@@ -1,5 +1,6 @@
+//数组形成的队列
 var ArrayQueue = function(){
-    this.items = [];
+    // this.items = [];
     this.n = 0;
     this.head = 0;//队头下标
     this.tail = 0;//队尾下标
@@ -19,5 +20,28 @@ proto.dequeue = function(){
     if(this.head==this.tail) return false;
     let res = this.items[head];
     ++head;
+    return res;
+}
+//数组形成的循环队列
+var CircularQueue = function(){
+    this.n = 0;
+    this.head = 0;
+    this.tail = 0;
+}
+var proto = CircularQueue.prototype;
+proto.init = function(capacity){
+    this.items = new Array(capacity);
+    this.n = capacity;
+}
+proto.enqueue = function(item){
+    if((this.tail+1)%this.n==this.head) return false;//队列满了
+    this.items[this.tail] = item;
+    this.tail = (this.tail+1)%this.n;
+    return true;
+}
+proto.dequeue = function(){
+    if(this.tail==this.head) return null;//队列为空
+    let res = this.items[this.head];
+    this.head = (this.head+1)%this.n;
     return res;
 }
